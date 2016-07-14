b9cb88fa6da0b8a546e8b6b7dd7f3171820ffe7b
Emily Liu <Emily@Emilys-MacBook-Pro-2.local>
2016-07-13 20:51:45 -0400

    fixed blinking

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 
 $(document).ready(function()
 {
+
+
+
+	// hover over words to cause words to blink
+	$("#twinkle_B").mouseenter(function()
+		{
+			$(this).fadeTo(400, 0.1).fadeTo(400, 1.0);
+		}).mouseleave(function()
+		{
+			$(this).fadeTo(400, 1.0);
+		});
+
+	$("#how_B").mouseenter(function()
+		{
+			$(this).fadeTo(400, 0.1).fadeTo(400, 1.0);
+		}).mouseleave(function()
+		{
+			$(this).fadeTo(400, 1.0);
+		});
+
+
+
+
+	// click twinkle_B to fade in last line
 	$("#twinkle_B").click(function()
 	{
-		$("#how_B").fadeIn();
-		setTimeout(function()
+		$("#how_B").fadeIn()
 			{
 				$("#how_B").text("How I wonder what you are");
-			}, 500);	
+			};
 	});
+
+
+
+
+
+
+
+
+
+
 });

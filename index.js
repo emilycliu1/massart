f7679c1df70b5f6ee2bf3e6393b21759e185283f
Emily Liu <Emily@Emilys-MacBook-Pro-2.local>
2016-07-17 16:34:45 -0400

    added a scroll to top arrow

--- index.js
+++ index.js
@@ -6,25 +6,6 @@ $(document).ready(function()
 	$("#form").hide();
 	$("#answer").hide();
 
-	/*
-	// first text will glow upon hover
-	$("#container_1").mouseenter(function()
-		{
-			$("#twinkle_A").
-			fadeTo(400, 0.3).
-			fadeTo(400, 1.0).
-
-			fadeTo(400, 0.3).
-			fadeTo(400, 1.0).
-
-			fadeTo(400, 0.3).
-			fadeTo(400, 1.0).
-
-			fadeTo(400, 0.3).
-			fadeTo(400, 1.0).
-		});
-	*/
-
 	// hover over text to expand, leave to go back to normal size
 	$(".text").mouseenter(function()
 		{
@@ -113,4 +94,56 @@ $(document).ready(function()
 
 		$("#answer").text(answerAppear).fadeIn(50).animate({fontSize: "60px"}, 200).animate({fontSize: "37px"}, 400); // this is the function, put it after the if/else because there has to be in input for answerAppear before the animations happen	
 	});
+
+
+
+
+
+	// 	ALL THIS STUFF IS FOR THE SCROLL TO TOP BUTTON
+
+	if ($("#back-to-top").length)
+	{
+
+		// This stuff concerns whether we see the arrow or not
+
+	    	var scrollTrigger = 700, // trigger is at 700 pixels aka almost the first picture size
+	        backToTop = function() // this is us defining the backToTop function
+	        {
+				var scrollTop = $(window).scrollTop();
+				if (scrollTop > scrollTrigger) // if the distance from the top is greater than the trigger distance
+				{
+				    $("#back-to-top").addClass('show'); // show the arrow
+				}else{
+				    $("#back-to-top").removeClass('show'); // if we're within 800 px of the top, don't show the arrow
+				}
+	        };
+
+			backToTop(); // this is that function we just defined above
+			// basically saying HEY! DO THIS FUNCTION! if you just refreshed, there should be no arrow
+
+			$(window).scroll(function() // when we scroll on the window, that function above will happen
+				{
+					backToTop();
+				});
+			// SCROLLING TRIGGERS THIS FUNCTION! after scrolling, arrow should show up
+
+		// This stuff concerns how the arrow functions upon clicking -- this is the real deal
+			$("#back-to-top").click(function (e){
+				e.preventDefault(); // prevents refreshing, as you will. literally who ever wants it to refresh??
+
+				$("html,body").animate(  // the body of the document animates
+					{
+					scrollTop: 0 // takes you to 0px on the document, aka the top
+					}, 600); // takes 600 milliseconds to do this
+			});
+	}
 });
+
+// la di da javascript is mean it never tells you exactly what's wrong
+// if you're going to have monospaced font you might as well have columns
+// help to identify which "unexpected identifier" or "token (" you're talking about
+// but it does cool shit and looks really pretty
+
+
+
+

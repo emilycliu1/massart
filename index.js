1079c24659253f7ac594b60b40db33201917ef62
Emily Liu <Emily@Emilys-MacBook-Pro-2.local>
2016-07-17 23:40:55 -0400

    finally done

--- index.js
+++ index.js
@@ -91,89 +91,44 @@ $(document).ready(function()
 		$("#answer").text(answerAppear).fadeIn(50).animate({fontSize: "60px"}, 200).animate({fontSize: "37px"}, 400); // this is the function, put it after the if/else because there has to be in input for answerAppear before the animations happen	
 	});
 
-// 	ALL THIS STUFF IS FOR THE SCROLL TO TOP BUTTON
+
+
+	// 	ALL THIS STUFF IS FOR THE SCROLLING BUTTONS
 
 	if ($(".returns").length)
 	{
-		// This stuff concerns how the arrow functions upon clicking -- this is the real deal
-
-		
 		$("#back_to_one").click(function (e){
 			e.preventDefault(); // prevents refreshing, as you will. literally who ever wants it to refresh??
-
 			$("html,body").animate(  // the body of the document animates
 				{
 				scrollTop: 0 // takes you to 0px on the document, aka the top of the first picture
 				}, 600); // takes 600 milliseconds to do this
 		});
 
-
 		$("#back_to_two").click(function (e){
-			e.preventDefault(); // prevents refreshing, as you will. literally who ever wants it to refresh??
-
-			$("html,body").animate(  // the body of the document animates
-				{
-				scrollTop: 860 // takes you to 860px on the document, aka the top of second picture
-				}, 600); // takes 600 milliseconds to do this
+			e.preventDefault();
+			$("html,body").animate({scrollTop: 860}, 600);
 		});
 
 
 		$("#back_to_three").click(function (e){
-			e.preventDefault(); // prevents refreshing, as you will. literally who ever wants it to refresh??
-
-			$("html,body").animate(  // the body of the document animates
-				{
-				scrollTop: 1880 // takes you to 1880px on the document, aka top of third picture
-				}, 600); // takes 600 milliseconds to do this
+			e.preventDefault();
+			$("html,body").animate({scrollTop: 1880}, 600);
 		});
 
 
 		$("#back_to_four").click(function (e){
-			e.preventDefault(); // prevents refreshing, as you will. literally who ever wants it to refresh??
-
-			$("html,body").animate(  // the body of the document animates
-				{
-				scrollTop: 2760 // takes you to 2760px on the document, aka top of fourth picture
-				}, 600); // takes 600 milliseconds to do this
+			e.preventDefault();
+			$("html,body").animate({scrollTop: 2760}, 600);
 		});
 
 
 		$("#back_to_five").click(function (e){
-			e.preventDefault(); // prevents refreshing, as you will. literally who ever wants it to refresh??
-
-			$("html,body").animate(  // the body of the document animates
-				{
-				scrollTop: 3800 // takes you to 3835px on the document, aka almost bottom
-				}, 600); // takes 600 milliseconds to do this
+			e.preventDefault();
+			$("html,body").animate({scrollTop: 3800}, 600);
 		});
 	}
 
-
-/*
-
-	// 	ALL THIS STUFF IS FOR THE SCROLL TO TOP BUTTON
-
-	if ($(".returns").length)
-	{
-		// This stuff concerns how the arrow functions upon clicking -- this is the real deal
-		$(".returns").click(function (e){
-			e.preventDefault(); // prevents refreshing, as you will. literally who ever wants it to refresh??
-
-			var triangle = $(".returns").val(); // THIS SI THE PROBLEM CHILD
-			var amountScroll = "";
-
-		
-			if triangle == "back_to_one"
-			{
-				amountScroll = 0;
-			}
-
-		});
-		
-		$("#body").scrollTop(amountScroll, 600); // scrolls to certain point on doc
-
-	}
-*/
 
 
 }); // this ends the document ready stuff -- DO NOT DELETE!!!
